0a31971162917fe4a1b5f18052f0e2b5d2ffb953
Hessler Inampuez <hesslerinampuez@gmail.com>
2022-12-08 11:54:31 -0500

    preliminary design of task component edited

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -4,12 +4,23 @@
     Description: This component will have the content of a task
 
  **********************/
-import React from 'react'
+import { Checkbox, Input, Stack, Text } from "@chakra-ui/react";
 
 const Task = () => {
   return (
-    <div>Task</div>
-  )
-}
+    <Stack direction="row" w="full">
+      <Input
+        variant="flushed"
+        size="sm"
+        colorScheme="cyan"
+        fontSize="15px"
+        placeholder="Enter description"
+      />
+      <Checkbox size="md" colorScheme="cyan" minW="90px">
+        <Text fontSize="12px">In progress</Text>
+      </Checkbox>
+    </Stack>
+  );
+};
 
-export default Task
+export default Task;
